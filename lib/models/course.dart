068f28a767e02190e5dfd9343b87ac21845afb26
punkushal <spycymagar@gmail.com>
2025-04-06 11:30:53 +0545

    course model added

---
+++ lib/models/course.dart
@@ -0,0 +1,19 @@
+class Course {
+  final String id;
+  final String title;
+  final String thumbnailUrl;
+  final String instructorName;
+  final String instructorImg;
+  final double duration;
+  final double price;
+
+  Course({
+    required this.id,
+    required this.title,
+    required this.thumbnailUrl,
+    required this.instructorName,
+    required this.instructorImg,
+    required this.duration,
+    required this.price,
+  });
+}
